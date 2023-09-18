9bb13e8c84a779cd28630a56d050396b143f3f36
hazel305 <hazel413@naver.com>
2023-09-18 13:09:37 +0900

    [FEAT]install & HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        return view('welcome');
+    }
+    
+      public function about(){
+        return '<h1>About Page</h1>';
+    }
+    
+     public function contact(){
+        return '<h1>contact Page</h1>';
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+// use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/store/{category?}/{item?}', function ($category=null, $item=null) {
+    // $category = request('category');
+    if(isset($category)){
+        if (isset($item)) {
+            return  '현재 스토어에서 '.strip_tags($category).'내'.$item.'카테고리를 보고 있습니다!';
+        }else{
+            return  '현재 스토어에서 '.strip_tags($category).'카테고리를 보고 있습니다!'; 
+        }
+        
+        
+    }else{
+        return  '현재 스토어를 모든 제품을 보고 있습니다!';
+    }
+});
+
+Route::get('/contact', function () {
+    //return view('welcome');
+    return '<h1>Contact Page</h1>';
+    
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
